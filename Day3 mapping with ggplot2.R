aac7e70968d73319ce87e08b578582466f6e49b9
Nkomo1997 <3650805@myuwc.ac.za>
2019-01-31 14:30:15 +0200

    day3 mapping

---
+++ Day3 mapping with ggplot2.R
@@ -0,0 +1,117 @@
+# mapping on ggplot
+# day 3
+# Thulisile Nkomo
+# 31 Jan 2019
+
+library(tidyverse)
+library(ggpubr)
+
+load("data/south_africa_coast.RData")
+load("data/sa_provinces.RData")
+load("data/rast_annual.RData")
+load("data/MUR.RData")
+load("data/MUR_low_res.RData")
+
+sst <- MUR_low_res #mere low resolution, the better the resolution the better graphs you create. disadvantage is that you haver more input and large data
+
+cols11 <- c("#004dcd", "#0068db", "#007ddb", "#008dcf", "#009bbc",
+            "#00a7a9", "#1bb298", "#6cba8f", "#9ac290", "#bec99a")
+
+
+ChickWeight <- datasets::ChickWeight
+
+ggplot(data = ChickWeight, aes(x = Time, y = weight)) +
+  geom_point()  
+
+ggplot(data = south_africa_coast, aes(x = lon, y = lat)) +
+  geom_point() 
+#difference in long (longitudinal) and lat (latitudinal)
+
+ggplot(data = south_africa_coast, aes(x = lon, y = lat)) +
+  geom_polygon(colour = "black", fill = "grey70", aes(group = group))
+
+ggplot(data = south_africa_coast, aes(x = lon, y = lat)) +
+  geom_polygon(colour = "black", fill = "grey70", aes(group = group)) +
+  geom_path(data = sa_provinces, aes(group = group)) # The province borders
+
+ggplot(data = south_africa_coast, aes(x = lon, y = lat)) +
+  geom_polygon(colour = "black", fill = "grey70", aes(group = group)) +
+  geom_path(data = sa_provinces, aes(group = group)) + 
+  coord_equal(xlim = c(15, 34), ylim = c(-36, -26), expand = 0) # Force lon/lat extent
+
+ggplot(data = south_africa_coast, aes(x = lon, y = lat)) +
+  geom_raster(data = sst, aes(fill = bins)) + # The ocean temperatures
+  geom_polygon(colour = "black", fill = "grey70", aes(group = group)) +
+  geom_path(data = sa_provinces, aes(group = group)) +
+  coord_equal(xlim = c(15, 34), ylim = c(-36, -26), expand = 0)
+
+ggplot(data = south_africa_coast, aes(x = lon, y = lat)) +
+  geom_raster(data = sst, aes(fill = bins)) +
+  geom_polygon(colour = "black", fill = "grey70", aes(group = group)) +
+  geom_path(data = sa_provinces, aes(group = group)) +
+  scale_fill_manual("Temp. (°C)", values = cols11) + # Set the colour palette
+  coord_equal(xlim = c(15, 34), ylim = c(-36, -26), expand = 0)
+
+
+#final map
+
+final_map <- ggplot(data = south_africa_coast, aes(x = lon, y = lat)) +
+  geom_raster(data = sst, aes(fill = bins)) +
+  geom_polygon(colour = "black", fill = "grey70", aes(group = group)) +
+  geom_path(data = sa_provinces, aes(group = group)) +
+  geom_tile(data = rast_annual, aes(x = lon, y = lat, fill = bins), 
+            colour = "white", size = 0.1) +
+  scale_fill_manual("Temp. (°C)", values = cols11) +
+  coord_equal(xlim = c(15, 34), ylim = c(-36, -26), expand = 0) +
+  scale_x_continuous(position = "top") + # Put x axis labels on top of figure
+  theme(axis.title = element_blank(), # Remove the axis labels
+        legend.text = element_text(size = 7), # Change text size in legend
+        legend.title = element_text(size = 7), # Change legend title text size
+        legend.key.height = unit(0.3, "cm"), # Change size of legend
+        legend.background = element_rect(colour = "white"), # Add legend background
+        legend.justification = c(1, 0), # Change position of legend
+        legend.position = c(0.35, 0.35)) # Fine tune position of legend
+
+
+
+#Mapping in
+#Load Africa map
+library(scales)
+library(ggsn)
+
+load("data/africa_map.RData")
+ggplot() + borders() + # The global shape file
+  coord_equal() #Equal sizing for lpn/lat
+
+
+sa_1 <- ggplot() +
+  borders(fill = "grey70", colour = "black") +
+  coord_equal(xlim = c(12, 36), ylim = c(-38, -22), expand = 0) # Force lon/lat extent
+
+sa_2 <- sa_1 +
+  annotate("text", label = "Atlantic\nOcean", 
+           x = 15.1, y = -32.0, 
+           size = 5.0, 
+           angle = 30, 
+           colour = "navy") +
+  annotate("text", label = "Indian\nOcean", 
+           x = 33.2, y = -34.2, 
+           size = 5.0, 
+           angle = 330, 
+           colour = "forestgreen")  
+
+sa_3 <- sa_2 +
+  scalebar(x.min = 32, x.max = 26, y.min = -36, y.max = -35, # Set location of bar
+           dist = 200, height = 1, st.dist = 0.8, st.size = 4, # Set particulars
+           dd2km = TRUE, model = "WGS84") + # Set appearance
+  north(x.min = 22.5, x.max = 25.5, y.min = -33, y.max = -31, # Set location of symbol
+        scale = 1.2, symbol = 16)
+
+sa_4 <- sa_3 +
+  annotation_custom(grob = ggplotGrob(africa_map),
+                    xmin = 20.9, xmax = 26.9,
+                    ymin = -30, ymax = -24)
+sa_4
+
+
+        
